602fcf1fa5a844f2c7cb59fda08d4cab970861c3
Alison Parisaca <alison-pq@outlook.com>
2024-09-16 17:49:11 -0400

    Add edit activity feature - restore state and redirect to details after changes

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -2,7 +2,7 @@ import { Button, Card, CardContent, CardDescription, CardHeader, CardMeta, Image
 import { useStore } from '../../../app/stores/store';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { observer } from 'mobx-react-lite';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 
 export default observer(function ActivityDetails() {
@@ -34,8 +34,8 @@ export default observer(function ActivityDetails() {
       </CardContent>
       <CardContent extra>
         <Button.Group widths='2'>
-          <Button basic color='blue' content='Edit' />
-          <Button basic color='grey' content='Cancel' />
+          <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit' />
+          <Button as={Link} to={'/activities'} basic color='grey' content='Cancel' />
         </Button.Group>
       </CardContent>
     </Card>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,22 @@
 import { Button, Form, Segment } from "semantic-ui-react";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { useNavigate, useParams } from "react-router-dom";
+import { Activity } from "../../../app/models/activity";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import {v4 as uuid } from 'uuid'; 
 
 export default observer(function ActivityForm() {
 
   const { activityStore } = useStore();
-  const { selectedActivity, createActivity, updateActivity, loading } = activityStore;
-  const initialState = selectedActivity ?? {
+  const { createActivity, updateActivity, 
+    loading, loadActivity, loadingInitial } = activityStore;
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [activity, setActivity] = useState<Activity>({
     id: '',
     title: '',
     category: '',
@@ -15,11 +24,20 @@ export default observer(function ActivityForm() {
     date: '',
     city: '',
     venue: '',
-  }
+  });
 
-  const [activity, setActivity] = useState(initialState);
+  useEffect(() => {
+    if (id) loadActivity(id).then(activity => setActivity(activity!));
+  }, [id, loadActivity]);
 
   function handleSubmit() {
+    if (activity.id) {
+      activity.id = uuid();
+      createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+    } else {
+      updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+
+    }
     activity.id ? updateActivity(activity) : createActivity(activity);
   }
 
@@ -27,6 +45,8 @@ export default observer(function ActivityForm() {
     const { name, value } = event.target;
     setActivity({ ...activity, [name]: value });
   }
+
+  if (loadingInitial) return <LoadingComponent content="Loading activity..." />
 
   return (
     <Segment clearing>
