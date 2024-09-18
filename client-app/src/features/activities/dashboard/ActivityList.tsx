ce12413638f06dc4bb5e61fb7c64f8b1a917346a
Alison Parisaca <alison-pq@outlook.com>
2024-09-18 15:09:41 -0400

    Remove unused code

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,7 +2,7 @@ import { Header } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import ActivityListItem from './ActivityListItem';
-import { Fragment } from 'react/jsx-runtime';
+import { Fragment } from 'react';
 
 export default observer(function ActivityList() {
   const { activityStore } = useStore();
