13fe1d19e325bbfd5405d8eb4232020fe1b02db2
Alison Parisaca <alison-pq@outlook.com>
2024-11-05 12:45:29 -0400

    Use Projection to get specific information from DB and configure mapper profile

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,3 +1,4 @@
+using Application.Activities;
 using AutoMapper;
 using Domain;
 
@@ -8,6 +9,26 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(
+                    activityDto => activityDto.HostUserName,
+                    option => option.MapFrom(
+                        activity => activity.Attendees.FirstOrDefault(activityAttendee => activityAttendee.IsHost).AppUser.UserName
+                    )
+                );
+            CreateMap<ActivityAttendee, Profiles.Profile>()
+                .ForMember(
+                    profile => profile.DisplayName,
+                    opt => opt.MapFrom(activityAttendee => activityAttendee.AppUser.DisplayName)
+                )
+                .ForMember(
+                    profile => profile.Usename,
+                    opt => opt.MapFrom(activityAttendee => activityAttendee.AppUser.UserName)
+                )
+                .ForMember(
+                    profile => profile.Bio,
+                    opt => opt.MapFrom(activityAttendee => activityAttendee.AppUser.Bio)
+                );
         }
     }
 }
